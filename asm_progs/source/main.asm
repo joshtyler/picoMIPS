eb9687aaba72c734206eac9de3dcc7c42bf7ab57
Joshua Tyler <josh.tyler@btinternet.com>
2017-05-04 12:20:08 +0100

    Pre move sim data to main section

--- asm_progs/source/main.asm
+++ asm_progs/source/main.asm
@@ -22,11 +22,11 @@ poll4:  JGZ     SW8     poll4
 
 //Begin Affine algorithm execution part 1
 //Note this could be optimised if some coefficients are repeated
-        MULTI   R1      R3		A11      // Multiply A11, and X1, store in R3
-        MULTI   R2      R4		A12      // Multiply A12, and Y1, store in R4
-        ADD     R3      R4              // Add R3 and R4, store in R4
+        MULTI   R1      R3		A11      // R3 = A11 * X1
+        MULTI   R2      R4		A12      // R4 = A12 * Y1
+        ADD     R3      R4              // R4 = R3 + R4
         LDI     R3		B1              // Store B2 in R3
-        ADD     R3      R4              // R4 = Y2 = B2 + (A21*X1) + (A22*Y1)
+        ADD     R3      R4              // R4 = Y2 = B2+(A21*X1)+(A22*Y1)
 
 //Begin output stage
 //No need to move R4 to LED as it is already connected
@@ -34,11 +34,11 @@ poll5:  JLEZ      SW8     poll5
         
 //Begin Affine algorithm execution part 2
 //Note this could be optimised if some coefficients are repeated
-        MULTI   R1      R3		A21      // Multiply A21, and X1, store in R3
-        MULTI   R2      R4		A22      // Multiply A22, and Y1, store in R4
-        ADD     R3      R4              // Add R3 and R4, store in R4
+        MULTI   R1      R3		A21      // R3 = A21 * X1
+        MULTI   R2      R4		A22      // R4 = A22 * Y1
+        ADD     R3      R4              // R4 = R3 + R4
         LDI     R3		B2              // Store B1 in R3
-        ADD     R3      R4              // R4 = X2 = B1 + (A11*X1) + (A12*Y1)
+        ADD     R3      R4              // R4 = X2 = B1+(A11*X1)+(A12*Y1)
 
 //Begin output stage
 //No need to move R4 to LED as it is already connected
